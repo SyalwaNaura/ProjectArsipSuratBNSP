74cc1c2622583de190d556f5b71cf5411aebb197
Syalwa Naura Rizquna <syalwanana123@gmail.com>
2021-10-24 22:25:57 +0700

    Final Commit

---
+++ db_surat.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2021 at 05:14 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_surat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id_admin` int(10) NOT NULL,
+  `nama_admin` varchar(50) NOT NULL,
+  `username_admin` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id_admin`, `nama_admin`, `username_admin`, `password`, `gambar`) VALUES
+(1, 'admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'syalwa.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_bagian`
+--
+
+CREATE TABLE `tb_bagian` (
+  `id_bagian` int(11) NOT NULL,
+  `nama_bagian` varchar(120) NOT NULL,
+  `username_admin_bagian` varchar(50) NOT NULL,
+  `password_bagian` varchar(50) NOT NULL,
+  `nama_lengkap` varchar(70) NOT NULL,
+  `tanggal_lahir_bagian` date NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp_bagian` varchar(12) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_bagian`
+--
+
+INSERT INTO `tb_bagian` (`id_bagian`, `nama_bagian`, `username_admin_bagian`, `password_bagian`, `nama_lengkap`, `tanggal_lahir_bagian`, `alamat`, `no_hp_bagian`, `gambar`) VALUES
+(9, 'UMUM', 'umum', 'b617726c7f45ecb196ef74881089fa17d90d7276', 'umum', '2017-11-17', 'smd', '333333333333', 'umum.jpg'),
+(10, 'D3 Manajemen Informatika', 'D3 Manajemen Informatika', '81afc10cba7b222d1c3d1d0ca75880f526af1927', 'D3 Manajemen Informatika', '2021-09-15', 'mlg', '24224242', 'D3 Manajemen Informatika.png'),
+(12, 'ADM.PEMB', 'adm pemb', 'df014d31bb48687f4da51001314fbbd2f1148052', 'adm pemb', '2021-04-15', 'malang', '089836323711', 'adm pemb.jpg'),
+(15, 'EKONOMI & SDA', 'ekonomi', 'df014d31bb48687f4da51001314fbbd2f1148052', 'ekonomi', '2021-06-03', 'mlg', '081299101309', 'ekonomi.jpg'),
+(20, 'INFRASTRUKTUR', 'infrastruktur', 'cc48a8c41985da07efc915fea8fc0ac3bec7f0e9', 'infrastruktur', '2017-12-01', 'smd', '083455253225', 'infrastruktur.jpg'),
+(23, 'D4 Teknologi Informasi', 'd4TI', 'teknologiinformasi', 'D4 Teknologi Informasi', '2021-09-28', 'malang', '089562345215', 'd4TI.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_suratkeluar`
+--
+
+CREATE TABLE `tb_suratkeluar` (
+  `id_suratkeluar` int(11) NOT NULL,
+  `tanggalkeluar_suratkeluar` datetime NOT NULL,
+  `kode_suratkeluar` varchar(10) NOT NULL,
+  `nomor_suratkeluar` varchar(45) NOT NULL,
+  `nama_bagian` varchar(70) NOT NULL,
+  `tanggalsurat_suratkeluar` date NOT NULL,
+  `kepada_suratkeluar` varchar(255) NOT NULL,
+  `perihal_suratkeluar` text NOT NULL,
+  `file_suratkeluar` varchar(255) NOT NULL,
+  `operator` varchar(50) NOT NULL,
+  `tanggal_entry` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_suratkeluar`
+--
+
+INSERT INTO `tb_suratkeluar` (`id_suratkeluar`, `tanggalkeluar_suratkeluar`, `kode_suratkeluar`, `nomor_suratkeluar`, `nama_bagian`, `tanggalsurat_suratkeluar`, `kepada_suratkeluar`, `perihal_suratkeluar`, `file_suratkeluar`, `operator`, `tanggal_entry`) VALUES
+(30, '2017-11-14 13:25:00', '915.1', '3454/ADM.PEMB/2017', 'ADM.PEMB', '2017-11-15', 'Walikota', 'Daftar Usulan Proyek', '2017-3454.pdf', 'admin', '2017-11-14 23:29:41'),
+(35, '2017-11-17 08:30:00', '821.1', '3452/TU/2017', 'TU', '2017-11-16', 'Kepala Bagian Lingkup Pemkot Samarinda', 'Pengangkatan Jabatan Pegawai Negeri ', '2017-3452.pdf', 'admin', '2017-11-16 17:35:35'),
+(93, '2021-10-18 21:17:00', '082', '3455', 'UMUM', '2021-10-21', 'Dosen Pengajar', 'Pemberian Dispensasi', '2021-3455.pdf', 'admin', '2021-10-24 21:18:09'),
+(94, '2021-10-22 21:59:00', '082', '3456', 'UMUM', '2021-10-21', 'Mhasiswa', 'Undangan', '2021-3456.pdf', 'admin', '2021-10-24 22:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_suratmasuk`
+--
+
+CREATE TABLE `tb_suratmasuk` (
+  `id_suratmasuk` int(11) NOT NULL,
+  `tanggalmasuk_suratmasuk` datetime NOT NULL DEFAULT current_timestamp(),
+  `kode_suratmasuk` varchar(10) NOT NULL,
+  `nomorurut_suratmasuk` varchar(7) NOT NULL,
+  `nomor_suratmasuk` varchar(25) NOT NULL,
+  `tanggalsurat_suratmasuk` date NOT NULL,
+  `pengirim` varchar(255) NOT NULL,
+  `kepada_suratmasuk` varchar(255) NOT NULL,
+  `perihal_suratmasuk` text NOT NULL,
+  `file_suratmasuk` varchar(255) NOT NULL,
+  `operator` varchar(50) NOT NULL,
+  `tanggal_entry` datetime NOT NULL DEFAULT current_timestamp(),
+  `disposisi1` varchar(50) NOT NULL,
+  `tanggal_disposisi1` datetime NOT NULL DEFAULT current_timestamp(),
+  `disposisi2` varchar(50) NOT NULL,
+  `tanggal_disposisi2` varchar(25) NOT NULL,
+  `disposisi3` varchar(50) NOT NULL,
+  `tanggal_disposisi3` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_suratmasuk`
+--
+
+INSERT INTO `tb_suratmasuk` (`id_suratmasuk`, `tanggalmasuk_suratmasuk`, `kode_suratmasuk`, `nomorurut_suratmasuk`, `nomor_suratmasuk`, `tanggalsurat_suratmasuk`, `pengirim`, `kepada_suratmasuk`, `perihal_suratmasuk`, `file_suratmasuk`, `operator`, `tanggal_entry`, `disposisi1`, `tanggal_disposisi1`, `disposisi2`, `tanggal_disposisi2`, `disposisi3`, `tanggal_disposisi3`) VALUES
+(6, '2017-09-26 10:00:00', '061', '4521', '061/4382/SJ', '2017-09-20', 'MENDAGRI RI', 'Jurusan Teknologi Informasi', 'Surat Edaran Tentang Mekanisme Layanan Administrasi Kemendagri\r\n', '2021-4521.pdf', 'admin', '2021-10-24 22:07:47', 'ASS.III', '2017-09-26 15:00:00', '', '1970-01-01 07:00:00', 'ORTAL', '2017-09-27 11:30:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `username_admin` (`username_admin`);
+
+--
+-- Indexes for table `tb_bagian`
+--
+ALTER TABLE `tb_bagian`
+  ADD PRIMARY KEY (`id_bagian`),
+  ADD UNIQUE KEY `username_admin_bagian` (`username_admin_bagian`);
+
+--
+-- Indexes for table `tb_suratkeluar`
+--
+ALTER TABLE `tb_suratkeluar`
+  ADD PRIMARY KEY (`id_suratkeluar`),
+  ADD UNIQUE KEY `nomor_suratkeluar` (`nomor_suratkeluar`);
+
+--
+-- Indexes for table `tb_suratmasuk`
+--
+ALTER TABLE `tb_suratmasuk`
+  ADD PRIMARY KEY (`id_suratmasuk`),
+  ADD UNIQUE KEY `nomorurut_suratmasuk` (`nomorurut_suratmasuk`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_bagian`
+--
+ALTER TABLE `tb_bagian`
+  MODIFY `id_bagian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `tb_suratkeluar`
+--
+ALTER TABLE `tb_suratkeluar`
+  MODIFY `id_suratkeluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+
+--
+-- AUTO_INCREMENT for table `tb_suratmasuk`
+--
+ALTER TABLE `tb_suratmasuk`
+  MODIFY `id_suratmasuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
